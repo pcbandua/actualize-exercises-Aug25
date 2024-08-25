a8b4f9857c0302bf58604763ed6fa462f31468ab
Paulo Bandua <pcbandua@gmail.com>
2024-08-25 10:56:17 -0700

    first day

---
+++ exercise1.rb
@@ -0,0 +1,13 @@
+# Given the array numbers = [5,2,6,1], find the sum of all the numbers in that array
+
+numbers = [5,2,6,1]
+
+sum = numbers.sum
+
+p sum
+
+# BONUS: Find the average of those numbers and make sure it’s correct (edited) 
+
+avg = sum / numbers.length
+
+p avg
